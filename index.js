11c1338afe374820089370d52add877891ef7877
Richard Wifall <hike@wifall.com>
2016-10-05 22:07:12 -0700

    Added support for using WOL as an option for powering on the TV. Cleaned up some formatting and incorrect text.

--- index.js
+++ index.js
@@ -1,94 +1,111 @@
 var request = require("request");
+var wol = require("wake_on_lan");
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
 
-  homebridge.registerAccessory("homebridge-sonybraviatv", "SonyBraviaTV", SonyBraviaTVAccessory);
+    homebridge.registerAccessory("homebridge-sonybraviatv", "SonyBraviaTV", SonyBraviaTVAccessory);
 }
 
 function SonyBraviaTVAccessory(log, config) {
-  this.log = log;
+    this.log = log;
 
-  this.name = config["name"];
-  this.psk = config["presharedkey"];
-  this.ipaddress = config["ipaddress"];
+    this.name = config["name"];
+    this.psk = config["presharedkey"];
+    this.ipaddress = config["ipaddress"];
+    this.macaddress = config["macaddress"];
 
 
-  this.service = new Service.Switch(this.name);
+    this.service = new Service.Switch(this.name);
 
-     this.service
+    this.service
         .getCharacteristic(Characteristic.On)
         .on('get', this.getOn.bind(this))
         .on('set', this.setOn.bind(this));
 
 }
 
-SonyBraviaTVAccessory.prototype.getOn = function(callback)
-{
-  this.log("Getting whether Sony TV is on...");
+SonyBraviaTVAccessory.prototype.getOn = function(callback) {
+    this.log("Getting whether Sony TV is on...");
 
-  var postData = JSON.stringify({
-    method: 'getPowerStatus',
-    params: [],
-    id: 1,
-    version: '1.0'
-  });
+    var postData = JSON.stringify({
+        method: 'getPowerStatus',
+        params: [],
+        id: 1,
+        version: '1.0'
+    });
 
-  request.post({
-    url: "http://"+this.ipaddress + "/sony/system",
-    headers: {'X-Auth-PSK': this.psk},
-    form: postData
-  }, function(err, response, body) {
+    request.post({
+        url: "http://" + this.ipaddress + "/sony/system",
+        headers: {
+            'X-Auth-PSK': this.psk
+        },
+        form: postData
+    }, function(err, response, body) {
 
-    if (!err && response.statusCode == 200) {
-      var json = JSON.parse(body);
-      var status = json.result[0].status;
-      this.log("TV status is %s", status);
-      var isOn = status == "active";
-      callback(null, isOn); // success
-    }
-    else {
-      if (response != null) {
-        this.log("Error getting TV status (status code %s): %s", response.statusCode, err);
-      }
-      else {
-        this.log("Error getting TV status: %s", err);
-      }
-      callback(err);
-    }
-  }.bind(this));
+        if (!err && response.statusCode == 200) {
+            var json = JSON.parse(body);
+            var status = json.result[0].status;
+            this.log("TV status is %s", status);
+            var isOn = status == "active";
+            callback(null, isOn); // success
+        } else {
+            if (response != null) {
+                this.log("Error getting TV status (status code %s): %s", response.statusCode, err);
+            } else {
+                this.log("Error getting TV status: %s", err);
+            }
+            callback(err);
+        }
+    }.bind(this));
 }
 
 SonyBraviaTVAccessory.prototype.setOn = function(value, callback) {
-  value = Boolean(value);
-  this.log("Set value to %s", value);
+    value = Boolean(value);
+    this.log("Set value to %s", value);
 
-  var postData = JSON.stringify({
-    method: 'setPowerStatus',
-    params: [{'status':value}],
-    id: 1,
-    version: '1.0'
-  });
+    if (value && this.macaddress) {
+        wol.wake(this.macaddress, function(error) {
+            if (error) {
+                // handle error
+                this.log("Error '%s' setting TV power state using WOL.", error);                
+                callback(error);
+            } else {
+                // done sending packets
+                callback();
+            }
+        }.bind(this));
+    } else {
 
+        var postData = JSON.stringify({
+            method: 'setPowerStatus',
+            params: [{
+                'status': value
+            }],
+            id: 1,
+            version: '1.0'
+        });
 
-  request.post({
-    url: "http://"+this.ipaddress + "/sony/system",
-    headers: {'X-Auth-PSK': this.psk},
-    form: postData
-  }, function(err, response, body) {
+        request.post({
+            url: "http://" + this.ipaddress + "/sony/system",
+            headers: {
+                'X-Auth-PSK': this.psk
+            },
+            form: postData
+        }, function(err, response, body) {
 
-    if (!err && response.statusCode == 200) {
-      callback(); // success
+            if (!err && response.statusCode == 200) {
+                callback(); // success
+            } else {
+                this.log("Error '%s' setting TV power state. Response: %s", err, body);
+                callback(err || new Error("Error setting TV power state."));
+            }
+        }.bind(this));
     }
-    else {
-      this.log("Error '%s' setting lock state. Response: %s", err, body);
-      callback(err || new Error("Error setting lock state."));
-    }
-  }.bind(this));
 }
 
 SonyBraviaTVAccessory.prototype.getServices = function() {
-  return [this.service];
+    return [this.service];
 }
